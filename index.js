1420130d16c26d38b675c519509c92c76ac5be94
itsDenMat <dennismateo93@gmail.com>
2022-08-14 23:05:54 -0700

    Add prompts

--- index.js
+++ index.js
@@ -1,7 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: function (answer) {
+            if (answer.length < 5 ) {
+                return console.log("Please enter a valid email address.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        vcalidate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You need to enter a valid title for your project.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the steps required to install your application.',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Provide information on how to properly use your application.',
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide guidelines on how other developers can contribute to your project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide any tests written for your applications. Please provide examples on how to run the tests.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license to apply to your project.',
+        choices: ['Apache 2.0', 'MIT'],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
